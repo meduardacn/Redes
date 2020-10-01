5d07ae515c25c56b7ba0d98dbee985536e7fb665
meduardcn <m.eduardacn@gmail.com>
2020-10-01 10:16:22 -0300

    mdif decode implemented

--- T1/mdif.py
+++ T1/mdif.py
@@ -3,39 +3,42 @@
 # one  | make a transition to thother side and same transition from the manchester encoding
 class MDIF():
     def encode(binary):
-        result = ""
-        if binary[0] == "0":
-            result += "+"
-            result += "-"
-        elif binary[0] == "1":
-            result += "-"
-            result += "+"
+        result = ''
+        if binary[0] == '0':
+            result += '+'
+            result += '-'
+        elif binary[0] == '1':
+            result += '-'
+            result += '+'
         for i in range(1,len(binary)):
-            if binary[i] == "0" and result[-1:] == "-":
-                result += "+"
-                result += "-"
-            elif binary[i] == "0" and result[-1:] == "+":
-                result += "-"
-                result += "+"
-            elif binary[i] == "1" and result[-1:] == "-":
-                result += "-"
-                result += "+"
-            elif binary[i] == "1" and result[-1:] == "+":
-                result += "+"
-                result += "-"
+            if binary[i] == '0' and result[-1:] == '-':
+                result += '+'
+                result += '-'
+            elif binary[i] == '0' and result[-1:] == '+':
+                result += '-'
+                result += '+'
+            elif binary[i] == '1' and result[-1:] == '-':
+                result += '-'
+                result += '+'
+            elif binary[i] == '1' and result[-1:] == '+':
+                result += '+'
+                result += '-'
         print(result)
 
-    def decode(binary):
-        result = ""
-        if binary[0] == "0":
-            result += "-"
-        elif binary[0] == "1":
-            result += "+"
-        for i in range(1,len(binary)):
-            if binary[i] == "0":
-                result += result[i-1]
-            elif result[i-1] == "-":
-                result += "+"
-            elif result[i-1] == "+":
-                result += "-"
+    def decode(sinal):
+        result = ''
+        if sinal[0] == '+' and sinal[1] == '-':
+            result += '0'
+        elif sinal[0] == '-' and sinal[1] == '+':
+            result += '1'
+        i = 2
+        while i < len(sinal):
+            if sinal[i-1] == '-' and sinal[i] == '-':
+                result += '1'
+            elif sinal[i-1] == '+' and sinal[i] == '+':
+                result += '1'
+            else:
+                result += '0'
+            i += 2
+        result = hex(int(result, 2)) 
         print(result)
