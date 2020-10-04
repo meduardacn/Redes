21700603379fecbd65299aef198a952235d6ae66
meduardcn <m.eduardacn@gmail.com>
2020-10-04 12:59:35 -0300

    attempt to implement decode for hdb3

--- T1/hdb3.py
+++ T1/hdb3.py
@@ -36,6 +36,41 @@ class HDB3():
             resultStr += elem
         print(resultStr)
 
-
     def decode(sinal):
-        return 0
+        infoBit = '-'
+        countZero = 0
+        verifyBit = '-'
+        i = 0
+        result = ['|' for _ in range(len(sinal))]
+        for i in range(0,len(sinal)):   
+            if sinal[i] != '0':
+                result[i] = '1'
+            else:
+                result[i] = '0'
+        change = 0
+        for i in range(0,len(sinal)):
+            if change == 1:
+                change = 0
+                continue
+            if result[i] == '1':
+                countZero = 0
+            if result[i] == '0':
+                countZero += 1
+                print(sinal[:i+1])
+                if countZero == 3 and (sinal[i+1] == sinal[i-3]):
+                    print("eae")
+                    change = 1
+                    result[i+1] = '0'
+                    countZero = 0
+                elif countZero == 2:
+                    print("entre")
+                    change = 1
+                    if sinal[i-3] == sinal[i+1]:
+                        result[i+1] = '0'
+                        result[i-2] = '0'
+                    countZero = 0
+                
+        resultStr = ''
+        for elem in result:
+            resultStr += elem
+        print(resultStr)
