2021d92ca87c2f8c04bde693057660cb001532c2
meduardcn <m.eduardacn@gmail.com>
2020-10-04 15:12:51 -0300

    first attempt encode 8b6t

--- T1/eightb6t.py
+++ T1/eightb6t.py
@@ -1,7 +1,44 @@
 class EightB6T():
+    def table():
+        f = open("eightb6t.txt", "r")
+        text = f.read()
+        text = text.split("\n")
+        table = dict()
+        for elem in text:
+            value = elem.split(" ")
+            table[value[0]] = value[1]
+        return(table)
+
     def encode(hexa):
-        print("encode")
-        return 0
+        print(hexa)
+        table = EightB6T.table()
+        i = 0
+        n = int(len(hexa)/2)
+        result = ""
+        resultPlus = 0
+        resultMinus = 0
+        for i in range(0,n):
+            key = hexa[i*2:(i+1)*2]
+            if resultMinus != resultPlus:
+                value = table[key]
+                newValue = ''
+                for elem in newValue:
+                    if elem == '+':
+                        newValue += '-'
+                    elif elem == '-':
+                        newValue += '+'
+                    elif elem == '0':
+                        newValue += '0'
+                result += newValue
+                resultMinus += newValue.count('-')
+                resultPlus += newValue.count('+')
+                print(resultMinus,resultPlus)
+            else:
+                result += table[key]
+                resultMinus += table[key].count('-')
+                resultPlus += table[key].count('+')
+                print(resultMinus,resultPlus)
+        print(result)  
 
     def decode():
         return 0
