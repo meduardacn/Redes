ff10084f1893985a39803cc1350896cc644d1960
meduardcn <m.eduardacn@gmail.com>
2020-11-01 20:36:34 -0300

    crc encode done

--- T2/CRC/crc_encoder.py
+++ T2/CRC/crc_encoder.py
@@ -1,30 +1,32 @@
 import sys 
 
-def main(binaryWord, binaryGenerator):
-    wordSlice = binaryWord[:len(binaryGenerator)]
+def encode(binaryLetter, binaryGenerator):
+    letterSlice = binaryLetter[:len(binaryGenerator)]
     i = len(binaryGenerator)
     zeros = "0"*len(binaryGenerator)
     xorResult = 0
-    while i < len(binaryWord):
-        if wordSlice[0] == "1":
-            xorResult = int(wordSlice, 2) ^ int(binaryGenerator,2)
+    while i < len(binaryLetter):
+        if letterSlice[0] == "1":
+            xorResult = int(letterSlice, 2) ^ int(binaryGenerator,2)
         else: 
-            xorResult = int(wordSlice, 2) ^ int(zeros, 2)
-        wordSlice = '{0:b}'.format(xorResult)
-        while len(wordSlice) != len(binaryGenerator):
-            wordSlice = "0" + wordSlice
-        wordSlice = wordSlice[1:] + binaryWord[i]
+            xorResult = int(letterSlice, 2) ^ int(zeros, 2)
+        letterSlice = '{0:b}'.format(xorResult)
+        while len(letterSlice) != len(binaryGenerator):
+            letterSlice = "0" + letterSlice
+        letterSlice = letterSlice[1:] + binaryLetter[i]
         i+=1
-    print(binaryWord, "word")
-    print(wordSlice, "resto final")
-    result = binaryWord[:len(binaryWord)-len(binaryGenerator)+1] + wordSlice[1:]
+    result = binaryLetter[:len(binaryLetter)-len(binaryGenerator)+1] + letterSlice[1:]
     result =  hex(int(result, 2))[2:]
-    print(result.upper())
+    return(result.upper())
 
-
+def main(word,binaryGenerator):
+    result = ""
+    letter = ""
+    for elem in [ord(ele) for sub in word for ele in sub]:
+        letter = bin(elem)[2:]         # transform letter into binary 
+        letter += "0"*(len(data[2])-1)  # add zeros 
+        result += encode(letter,binaryGenerator)
+    print(result)
+    
 data  = sys.argv
-word = ""
-for elem in [ord(ele) for sub in data[1] for ele in sub]:
-    word += bin(elem)[2:]       # transform each letter into binary 
-word += "0"*(len(data[2])-1)    # add zeros to the messege final
-main(word,data[2])
+main(data[1],data[2])
