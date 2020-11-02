b19170ae89fc3c83fe50645b2151a37a0025267f
meduardcn <m.eduardacn@gmail.com>
2020-11-01 22:44:27 -0300

    crc encode fixed

--- T2/CRC/crc_encoder.py
+++ T2/CRC/crc_encoder.py
@@ -1,11 +1,10 @@
 import sys 
-
 def encode(binaryLetter, binaryGenerator):
     letterSlice = binaryLetter[:len(binaryGenerator)]
     i = len(binaryGenerator)
     zeros = "0"*len(binaryGenerator)
     xorResult = 0
-    while i < len(binaryLetter):
+    while i <= len(binaryLetter):
         if letterSlice[0] == "1":
             xorResult = int(letterSlice, 2) ^ int(binaryGenerator,2)
         else: 
@@ -13,7 +12,8 @@ def encode(binaryLetter, binaryGenerator):
         letterSlice = '{0:b}'.format(xorResult)
         while len(letterSlice) != len(binaryGenerator):
             letterSlice = "0" + letterSlice
-        letterSlice = letterSlice[1:] + binaryLetter[i]
+        if i != len(binaryLetter):
+            letterSlice = letterSlice[1:] + binaryLetter[i]
         i+=1
     result = binaryLetter[:len(binaryLetter)-len(binaryGenerator)+1] + letterSlice[1:]
     result =  hex(int(result, 2))[2:]
@@ -21,12 +21,12 @@ def encode(binaryLetter, binaryGenerator):
 
 def main(word,binaryGenerator):
     result = ""
-    letter = ""
+    binLetter = ""
     for elem in [ord(ele) for sub in word for ele in sub]:
-        letter = bin(elem)[2:]         # transform letter into binary 
-        letter += "0"*(len(data[2])-1)  # add zeros 
-        result += encode(letter,binaryGenerator)
+        binLetter = bin(elem)[2:]         # transform letter into binary 
+        binLetter += "0"*(len(data[2])-1)  # add zeros 
+        result += encode(binLetter,binaryGenerator)
     print(result)
-    
+
 data  = sys.argv
 main(data[1],data[2])
