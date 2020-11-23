6c4fbd4cb3e8e108600cb526e2426b6348755691
meduardcn <m.eduardacn@gmail.com>
2020-11-23 15:58:19 -0300

    added tests

--- T3/test.sh
+++ T3/test.sh
@@ -8,21 +8,25 @@ echo ""
 python3 simulador.py a.txt n1 n3 helloworld
 echo ""
 echo "TRABALHO"
-python3 simulador.py topo1.txt n1 n2 redes
+python3 simulador.py topo1.txt n1 n2 redes # 
 echo ""
-python3 simulador.py topo1.txt n1 n3 redes
+python3 simulador.py topo1.txt n1 n3 redes #
 echo ""
-python3 simulador.py topo1.txt n2 n3 computador
+python3 simulador.py topo1.txt n2 n3 computador #
 echo ""
-python3 simulador.py topo2.txt n1 n2 computador
+python3 simulador.py topo2.txt n1 n2 computador #
 echo ""
-python3 simulador.py topo2.txt n1 n3 computador
+python3 simulador.py topo2.txt n1 n3 computador #
 echo "" 
-python3 simulador.py topo2.txt n1 n4 computador
+python3 simulador.py topo2.txt n1 n4 computador #
 echo ""
-python3 simulador.py topo2.txt n2 n3 computador
+python3 simulador.py topo2.txt n2 n3 computador #
 echo ""
-python3 simulador.py topo2.txt n2 n4 computador
+python3 simulador.py topo2.txt n2 n4 computador #
 echo ""
-python3 simulador.py topo2.txt n3 n4 computador
+python3 simulador.py topo2.txt n3 n4 computador #
 echo ""
+# bug with ttl, r => n tll should be 7
+# line 298 must be return 
+# print all mac value
+# r => r comunication
