4b8a5a81db696b1a5f99ee701f7f83bdf2967acc
meduardcn <m.eduardacn@gmail.com>
2020-11-23 14:56:03 -0300

    added tests

--- T3/test.sh
+++ T3/test.sh
@@ -1,3 +1,4 @@
+#sh test.sh  
 python3 simulador.py a.txt n1 n2 hello
 echo ""
 python3 simulador.py a.txt n1 n2 helloworld
@@ -5,3 +6,23 @@ echo ""
 python3 simulador.py a.txt n1 n3 hello
 echo ""
 python3 simulador.py a.txt n1 n3 helloworld
+echo ""
+echo "TRABALHO"
+python3 simulador.py topo1.txt n1 n2 redes
+echo ""
+python3 simulador.py topo1.txt n1 n3 redes
+echo ""
+python3 simulador.py topo1.txt n2 n3 computador
+echo ""
+python3 simulador.py topo2.txt n1 n2 computador
+echo ""
+python3 simulador.py topo2.txt n1 n3 computador
+echo "" 
+python3 simulador.py topo2.txt n1 n4 computador
+echo ""
+python3 simulador.py topo2.txt n2 n3 computador
+echo ""
+python3 simulador.py topo2.txt n2 n4 computador
+echo ""
+python3 simulador.py topo2.txt n3 n4 computador
+echo ""
